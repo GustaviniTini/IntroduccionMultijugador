6e7c83e0ccbadd1435013e0204985ca6688050fb
GustaviniTini <gustavoegn2101@gmail.com>
2023-11-22 16:05:14 -0500

    Initial commit

---
+++ Assets/LogicaRed.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class logicaRed : MonoBehaviourPunCallbacks
+{
+    public static logicaRed instancia;
+
+    void Awake()
+    {
+        instancia = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Felicidades estas conectado");
+    }
+}
